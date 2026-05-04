91bb750776cc25fbc9ef4a732feecc8d4fe83dd9
araragi0040-jpg <araragi0040@gmail.com>
2026-05-04 21:23:18 +0900

    Update EventInfo.tsx

--- src/components/EventInfo.tsx
+++ src/components/EventInfo.tsx
@@ -14,36 +14,38 @@ export function EventInfo({ event, performerCount }: EventInfoProps) {
           <dt>開催日</dt>
           <dd>{formatDate(event.date)}</dd>
         </div>
+
         <div>
           <dt>会場</dt>
           <dd>{event.venue}</dd>
         </div>
+
         <div>
           <dt>回数</dt>
           <dd>LIFE ARTS LIVE 第{event.number}回</dd>
         </div>
+
         <div>
           <dt>出演者</dt>
           <dd>{performerCount}組</dd>
         </div>
-        
-           {event.venueUrl && (
-            <div className="event-info-row">
-              <dt>会場詳細</dt>
-              <dd>
-                <a
-                  href={event.venueUrl}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="event-info-link"
-                >
-                  会場情報を見る
-                </a>
-              </dd>
-            </div>
-          )}
-        </div>
-      </div>
-    </section>
+
+        {event.venueUrl && (
+          <div>
+            <dt>会場詳細</dt>
+            <dd>
+              <a
+                href={event.venueUrl}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="event-info-link"
+              >
+                会場情報を見る
+              </a>
+            </dd>
+          </div>
+        )}
+      </dl>
+    </div>
   );
 }
