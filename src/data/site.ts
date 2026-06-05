e4aa2b7109c1a22fd4b8952f5471af5649e19000
araragi0040-jpg <araragi0040@gmail.com>
2026-06-06 00:06:41 +0900

    Update site.ts

--- src/data/site.ts
+++ src/data/site.ts
@@ -6,7 +6,8 @@ export const siteData = {
     "ここでは、上手いかどうかよりも、その人自身が表現として立ち上がることを大切にしています。",
   aboutTitle: "LIFE ARTS LIVEとは",
   aboutText:
-    "上手さや完成度だけでは測れない、その人自身の想いや内側が表現としてあふれるライブイベントです。\n音楽、言葉、身体表現、物語。ジャンルに縛られず、それぞれの形で「自分」を表現する場です。",
+    "aboutText:
+  "LIFE ARTS LIVEは、大阪で開催しているライブイベントです。\n\n音楽、言葉、身体表現、物語など、ジャンルに縛られず、それぞれの形で「自分」を表現する場。\n\n上手い下手や完成度だけでは測れない、その人自身の想いや内側が表現として立ち上がる瞬間を大切にしています。",",
   instagramUrl: "https://www.instagram.com/life_arts_live",
   hostName: "しゅう",
   hostInstagramUrl: "https://www.instagram.com/shuhe0040",
